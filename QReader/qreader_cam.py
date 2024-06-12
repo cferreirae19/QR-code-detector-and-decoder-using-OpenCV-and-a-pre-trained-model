afcea2fb1412c00a93ba332de51b1292aaa65c2e
cferreirae19 <cferreiraescudero@gmail.com>
2024-06-12 16:09:24 +0200

    Updated version

--- QReader/qreader_cam.py
+++ QReader/qreader_cam.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from qreader import QReader
 
 # Open the camera
@@ -15,6 +16,8 @@ while True:
     # Capture every frame
     ret, frame = capture.read()
     
+    cv2.imshow("Detect QR Code from Webcam", frame)
+    
     # Get the image that contains the QR code
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
@@ -23,9 +26,13 @@ while True:
         break
     
     # Use the detect_and_decode function to get the decoded QR data
-    decoded_text = qreader.detect_and_decode(image=image)
-    if len(decoded_text) != 0:
-        print(decoded_text)
+    decoded_text = qreader.detect_and_decode(image=image, return_detections=True)
+    if len(decoded_text[0]) != 0:
+        bbox = decoded_text[1][0]['bbox_xyxy']
+        start_point = (int(bbox[0]), int(bbox[1]))
+        end_point = (int(bbox[2]), int(bbox[3]))
+        cv2.rectangle(frame, start_point, end_point, (255, 0, 255), 3)
+        cv2.imshow("Detect QR Code from Webcam", frame)
     
     # Press 'q' to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
